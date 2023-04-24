33227ebed137f70152b693ff69b091be28a9d36d
Your Name <gahboubwafa@gmail.com>
2023-04-24 06:27:23 -0700

    wafae

--- _printf.c
+++ _printf.c
@@ -10,46 +10,43 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int count = 0;
-	va_list args;
-
-	va_start(args, format);
-	if (format == NULL)
-		return (-1);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-
-				putcharwt(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char*);
-
-				while (*s)
-				{
-					putcharwt(*s), s++, count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				putcharwt('%');
-				count++;
-			}
-		}
-		else
-		{
-			putcharwt(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+unsigned int count = 0;
+va_list args;
+va_start(args, format);
+if (format == NULL)
+return (-1);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+putcharwt(c);
+count++;
+}
+else if (*format == 's')
+{
+char *s = va_arg(args, char*);
+while (*s)
+{
+putcharwt(*s), s++, count++;
+}
+}
+else if (*format == '%')
+{
+putcharwt('%');
+count++;
+}
+}
+else
+{
+putcharwt(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }
